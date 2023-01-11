0d1c40a37c547e0f787a2416b95e7dd8e66af82f
Luiz <luizhmartins11@gmail.com>
2023-01-11 15:42:24 -0300

    Estudo sobre Services

--- src/app/components/list-render/list-render.component.ts
+++ src/app/components/list-render/list-render.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { Animal } from 'src/app/Animal';
+import { ListService } from 'src/app/services/list.service';
 @Component({
   selector: 'app-list-render',
   templateUrl: './list-render.component.html',
@@ -12,8 +13,12 @@ export class ListRenderComponent {
     { name: 'Teteu', type: 'Bird', age: 2 },
     { name: 'Astolfo', type: 'Hamster', age: 1 },
   ];
+  constructor(private listService: ListService) {}
   animalDetails = '';
   showAge(animal: Animal) {
     this.animalDetails = `O pet ${animal.name} tem ${animal.age} ano(s)`;
+  }
+  removeAnimal(animal: Animal) {
+    this.animals = this.listService.remove(this.animals, animal);
   }
 }
